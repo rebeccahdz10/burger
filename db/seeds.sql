353f9331750f7cf5aac08e3638c54e6895d924c1
Rebecca H <rhernandez@skyslope.com>
2019-09-23 19:10:58 -0700

    files n stuff

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("CheeseBurger", "false");
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Barbeque", "false");
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Veggie", "false");   
